77f1783e2045f4d030bd114ef4b50dbb1ad715c4
Per Buer <per.buer@gmail.com>
2023-09-17 10:18:24 +0200

    refactor

--- mon/main.go
+++ mon/main.go
@@ -10,54 +10,65 @@ import (
 	"time"
 )
 
+// Monitor continuously checks and updates the DNS record if the external IP changes.
 func Monitor(ctx context.Context, c config.Config) error {
-	externalAddr, err := ip.ExternalIpV4()
-	if err != nil {
-		return fmt.Errorf("ip.ExternalIpV4: %w", err)
-	}
 	client, err := getClient(c)
 	if err != nil {
-		return fmt.Errorf("getClient: %w", err)
+		return err
 	}
+	externalAddr, err := ip.ExternalIpV4()
+	if err != nil {
+		return fmt.Errorf("retrieving external IP: %w", err)
+	}
+
 	id, existing, err := getLolIdName(client, c.Username, c.Hostname)
 	if err != nil {
-		return fmt.Errorf("getLolAddr: %w", err)
+		return err
 	}
 	if existing != externalAddr {
-		err := updateLolAddr(client, id, c.Hostname, externalAddr, c.Username)
+		err = updateDNS(client, id, c, externalAddr)
 		if err != nil {
-			return fmt.Errorf("updateLolAddr: %w", err)
+			return err
 		}
-		slog.Info("Updated DNS record", "hostname", c.Hostname, "username", c.Username, "externalAddr", externalAddr)
 	}
-	refreshTicker := time.NewTicker(30 * time.Minute)
-	for {
-
-		select {
-		case <-refreshTicker.C:
-			newAddr, err := ip.ExternalIpV4()
-			if err != nil {
-				slog.Warn("ip.ExternalIpV4 returned an error", "error", err)
-				continue
-			}
-			if newAddr != externalAddr {
-				slog.Info("Updating DNS record", "hostname", c.Hostname, "username", c.Username, "externalAddr", newAddr)
-				err := updateLolAddr(client, id, c.Hostname, newAddr, c.Username)
-				if err != nil {
-					slog.Warn("updateLolAddr failed", "error", err)
-					continue
-				}
-				externalAddr = newAddr
-			}
-		case <-ctx.Done():
-			return nil
-		}
-
-	}
+	return monitorIPChanges(ctx, client, id, c, externalAddr)
 }
 
 func getClient(c config.Config) (*omglol.Client, error) {
 	return omglol.NewClient(c.Email, c.ApiKey)
+}
+
+func updateDNS(c *omglol.Client, id int64, configData config.Config, externalAddr string) error {
+	err := updateLolAddr(c, id, configData.Hostname, externalAddr, configData.Username)
+	if err != nil {
+		return fmt.Errorf("updating DNS record: %w", err)
+	}
+	slog.Info("Updated DNS record", "hostname", configData.Hostname, "username", configData.Username, "externalAddr", externalAddr)
+	return nil
+}
+
+func monitorIPChanges(ctx context.Context, client *omglol.Client, id int64, c config.Config, currentAddr string) error {
+	refreshTicker := time.NewTicker(30 * time.Minute)
+	for {
+		select {
+		case <-refreshTicker.C:
+			newAddr, err := ip.ExternalIpV4()
+			if err != nil {
+				slog.Warn("retrieving external IP", "error", err)
+				continue
+			}
+			if newAddr != currentAddr {
+				err := updateDNS(client, id, c, newAddr)
+				if err != nil {
+					slog.Warn("failed to update DNS record", "error", err)
+					continue
+				}
+				currentAddr = newAddr
+			}
+		case <-ctx.Done():
+			return nil
+		}
+	}
 }
 
 // updateLolAddr updates the DNS record for the given domain with the new name.
